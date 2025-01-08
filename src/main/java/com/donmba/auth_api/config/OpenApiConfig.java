682a43edbc82ab88393c79988b9d7341f91a084b
Donald <donaldmbara@gmail.com>
2025-01-08 23:52:21 +0200

    added prod/dev url with https

--- src/main/java/com/donmba/auth_api/config/OpenApiConfig.java
+++ src/main/java/com/donmba/auth_api/config/OpenApiConfig.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,6 +19,8 @@ public class OpenApiConfig {
                 .title("Auth API")
                 .version("v1")
                 .description("API for user authentication and management"))
+            .addServersItem(new Server().url("https://auth-api-development.up.railway.app"))  // Development server
+            .addServersItem(new Server().url("https://auth-api-prod-54a7.up.railway.app"))  // Production server
             .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
             .components(new io.swagger.v3.oas.models.Components()
                 .addSecuritySchemes(securitySchemeName,
