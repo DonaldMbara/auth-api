ea91c34153565e695e535b8fb7bd262b4f94a517
Donald <donaldmbara@gmail.com>
2025-01-08 23:20:41 +0200

    Update JwtAuthenticationFilter.java

--- src/main/java/com/donmba/auth_api/security/JwtAuthenticationFilter.java
+++ src/main/java/com/donmba/auth_api/security/JwtAuthenticationFilter.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.util.Collections;
