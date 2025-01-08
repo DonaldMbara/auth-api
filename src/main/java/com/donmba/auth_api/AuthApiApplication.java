e12d1e1c156d60d4a3dd98f7df1890a810287813
DonaldMbara <donaldmbara@gmail.com>
2025-01-08 11:11:24 +0200

    updated configs

--- src/main/java/com/donmba/auth_api/AuthApiApplication.java
+++ src/main/java/com/donmba/auth_api/AuthApiApplication.java
@@ -1,13 +1,8 @@
 package com.donmba.auth_api;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 
-//TODO: remove exclude = {SecurityAutoConfiguration.class}
-// once api-gateway is implemented
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class AuthApiApplication {
 
 
